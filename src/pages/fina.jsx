3391eaa70f1d98d72db99f5dd2dacbef01e6be77
mushawwir <mushawwirrd7@gmail.com>
2025-11-18 15:02:58 +0700

    add style

--- src/pages/fina.jsx
+++ src/pages/fina.jsx
@@ -5,6 +5,14 @@ function Fina() {
     const [spending, setSpending] = useState()
     const [message, setMessage] = useState(null)
 
+    function realNumber(strg) {
+        return Number(strg.replace(/\./, ""))
+    }
+
+    function decimal(nmbr) {
+        if (nmbr === null) return ""
+        return nmbr.toString(n => n.replace(/\b/, "."))
+    }
 
     function submitHandle(e) {
         e.preventDefault()
@@ -21,42 +29,63 @@ function Fina() {
 
     return (
         <div>
-            <div>
 
-                <div>
+            <div className="absolute z-10">
+                <nav className=" px-14 py-6 w-screen">
+                    <div>
+                        <a href="#" className="font-semibold text-xl text-white">FINA</a>
+                    </div>
+                </nav>
+
+            </div>
+
+            <div className=" flex flex-col items-center justify-center bg-gradient-to-b from-blue-800 to-blue-600 h-screen ">
+                
+                <div className="mb-8">
+                    <h1 className="text-4xl text-white font-bold">Cek Kondisi Kesehatan Keuangan Anda</h1>
+                </div>
+                
+                <div className=" px-12 py-10 rounded-2xl bg-white shadow-lg w-[360px] h-[384px]">
                     <form onSubmit={submitHandle}>
 
-                        <div>
-                            <label className="block">Pemasukan</label>
-                            <input
-                                type="text"
-                                value={income}
-                                onChange={e => setIncome(Number(e.target.value))}
-                                className="border p-2 rounded-md" />
+                        <div className="flex flex-col items-center justify-center">
+
+                            <div className="flex flex-col  items-start justify-start w-full mb-5">
+                                <label className="text-gray-500 mb-1">Pemasukan</label>
+                                <input
+                                    type="text"
+                                    value={income}
+                                    onChange={e => setIncome(Number(e.target.value))}
+                                    className="border p-4 rounded-xl w-full" />
+                            </div>
+
+                            <div className="flex flex-col items-start justify-start w-full">
+                                <label className=" text-gray-500 mb-1">Pengeluaran</label>
+                                <input
+                                    type="text" value={spending}
+                                    onChange={e => setSpending(Number(e.target.value))}
+                                    className="border p-4 rounded-xl w-full " />
+                            </div>
+
                         </div>
 
-                        <div>
-                            <label className="block">Pengeluaran</label>
-                            <input
-                                type="text" value={spending}
-                                onChange={e => setSpending(Number(e.target.value))}
-                                className="border p-2 rounded-md" />
+
+                        <div className="flex items-center justify-center mt-8">
+                            <button className="py-3 bg-blue-500 rounded-full w-48 text-white">Diagnosa</button>
                         </div>
 
-                        <div>
-                            <button>Diagnosa</button>
+                        <div className="flex items-center justify-center text-center mt-4">
+                            {message == "Sehat" && <p className="text-green-500">Kondisi keuangan kamu sudah sehat</p>}
+                            {message == "Belum input" && <p>Kamu belum input apa - apa</p>}
+                            {message == "Tidak sehat" && <p className="text-red-600">Kondisi keuangan kamu belum sehat</p>}
                         </div>
+
+
 
                     </form>
                 </div>
 
-                <div>
 
-                    {message == "Sehat" && <p>Uang anda sehat</p>}
-                    {message == "Belum input" && <p>Silahkan masukkan pengeluaran dan pemasukan anda</p>}
-                    {message == "Tidak sehat" && <p>Uang anda tidak sehat</p>}
-
-                </div>
 
             </div>
         </div>
